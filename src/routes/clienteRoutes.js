b9c6d88df4cd6c7678c35180c4f4a706c0716681
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2024-02-24 21:16:06 -0300

    alterado metodo get para post no buscar por cpfcnpj

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -6,15 +6,10 @@ const { validateIdParam } = require('../middlewares/validateParam');
 const { idParam: idParamSchema } = require('../schemas/idParam');
 const { findCliente, createCliente, updateCliente } = require('../controllers/clienteController');
 
-routes.get('/', validateBody(clienteSchema), findCliente)
+routes.post('/busca', validateBody(clienteSchema), findCliente)
 
 routes.post('/', createCliente)
 
 routes.post('/:id', validateIdParam(idParamSchema), updateCliente)
-/* ((req, res) => {
-  const { cpfcnpj } = req.body;
-  console.log(cpfCnpj);
-  return res.status(200).json({ mensage: 'Rota para buscar cliente no banco' });
-}); */
 
 module.exports = routes;
